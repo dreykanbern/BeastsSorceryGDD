16b613298be83025d14ed62f880761ebc504d54b
Daniil Kazachkin <83606022+dreykanbern@users.noreply.github.com>
2024-08-20 04:13:55 +0400

    init project

---
+++ BeastsSorcery/Source/BeastsSorcery/BeastsSorcery.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BeastsSorcery.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeastsSorcery, "BeastsSorcery" );
